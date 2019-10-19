c37edab12ef21d261dfb50522a9cf7cf34a145a6
silvodesigns <kevsilvo@gmail.com>
2019-10-19 03:09:59 -0400

    rendering state

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
+//My state holding my list of TODOs
+const initialState = [
+  
+  {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 1
+  }
+
+]
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {...state, todo: action.payload};
+    default:
+      return state;
+  }
+}
+
+
 function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
+
   return (
     <div className="App">
       <h1>Todo App</h1>
-     
+      {state.map(item => <p>{item.item}</p>)}
     </div>
   );
 }
